475cca0e5ef67df79539fbcc28d30727604b4ffe
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-03-02 16:41:22 +0500

    Add multiple functions with print statements

---
+++ main.py
@@ -0,0 +1,64 @@
+# 1
+def salomlashuv():
+    print("Salom dunyo")
+
+salomlashuv()
+
+# 2
+def raqam():
+    print(100)
+
+raqam()
+
+# 3
+def xabar():
+    print('Siz muvaffaqiyat sari yurasiz')
+
+xabar()
+
+# 4
+def matn():
+    print('Python' * 3)
+
+matn()
+
+# 5
+def hisob():
+    print(5 + 9)
+
+hisob()
+
+
+# 6
+def xabar():
+    print("Hozir tong!")
+
+
+xabar()
+
+# 7
+def fikr():
+    print("Organish kelajak")
+
+fikr()
+
+
+# 8
+def xabar():
+    print('Dastur ishga tushish')
+
+
+xabar()
+
+
+# 9
+def salom():
+    print("Bugn yaxshi kun")
+
+salom()
+
+# 10
+def chaqrish():
+    print("Xush kelibsz")
+
+chaqrish()
